987978846f9497337b9a5f47c620182917c1f552
James Strassburg <jstrassburg@gmail.com>
2013-01-20 14:02:13 -0600

    First commit

---
+++ euler.py
@@ -0,0 +1,252 @@
+# library of functions for Project Euler problems
+from math import sqrt, factorial
+
+# a range that works for long values
+def bigRange(start, stop, step = 1):
+    x = start
+    while x < stop:
+        yield x
+        x = x + step
+
+# a fast reverse range that works for long values
+def reverseBigRange(start, stop = 0, step = 1):
+    x = start
+    while x > stop:
+        yield x
+        x = x - step
+
+# a Fibonacci sequence generator
+def fibonacci():
+    a, b = 1, 2
+    while True:
+        yield a
+        a, b = b, a + b
+
+# a generator for prime numbers, keeps going until you stop pulling
+def generatePrimes(start = 2):
+    current = start
+    if current == 2 or isPrime(current):
+        yield current # 2
+    current += 1
+    yield current # 3
+    while 1:
+        current += 2 # ignore even numbers
+        if isPrime(current):
+            yield current
+
+# a prime number sieve using the Eratostenes algorithm
+def getPrimesTo(n):
+    primes = set(xrange(3,n, 2))
+    primes.add(2)
+    for p in xrange(3, n/2, 2):
+        if p in primes:
+            for multiple in getMultiplesTo(p, n):
+                primes.discard(multiple)
+    return primes
+
+# a generator for triangle numbers
+# 1, +=2, +=3, += 4, etc...
+# 1, 3, 6, 10, 15, etc...
+def generateTriangleNumbers():
+    current = 1
+    triangle = 0
+    while True:
+        triangle += current
+        current += 1
+        yield triangle
+
+# determines if x is a triangle number
+def isTriangle(x):
+    n = (sqrt(8*x+1)-1)/2.0
+    return n == int(n)
+
+# a generator for pentagonal numbers
+# Pn = (n(3n-1))/2
+# 1,5,12,22,35,51,70,...
+def generatePentagonalNumbers():
+    n = 1
+    while True:
+        pentagonal = (n*(3*n-1))/2
+        yield pentagonal
+        n += 1
+
+# determines if x is a pentagonal number
+# Pn = (n(3n-1))/2
+def isPentagonal(x):
+    n = (sqrt(24*x+1)+1)/6.0
+    return n == int(n)
+
+# a generator for pentagonal numbers
+# Hn = n(2n - 1)
+# 1,6,15,28,45,...
+def generateHexagonalNumbers():
+    n = 1
+    while True:
+        hexagonal = n*(2*n - 1)
+        yield hexagonal
+        n += 1
+
+# determines if x is a hexagonal number
+def isHexagonal(x):
+    n = (sqrt(8*x+1)+1)/4.0
+    return n == int(n)
+
+# generates an iterative sequence starting at start
+# n => n/2 (when n is even)
+# n => 3n+1 (when n is odd)
+def getIterativeSequence(start):
+    current = start
+    sequence = []
+    while current > 1:
+        sequence.append(current)
+        if isEven(current): current /= 2
+        else: current = 3*current + 1
+    sequence.append(current)
+
+    return sequence
+
+# filter for even numbers
+def isEven(x):
+    return x % 2 == 0
+
+# filter for odd numbers
+def isOdd(x):
+    return not isEven(x)
+
+# filter for prime numbers
+def isPrime(n):
+    # shortcuts
+    if n == 0 or n == 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    if n > 5 and n % 5 == 0:
+        return False
+
+    for x in bigRange(3, sqrt(n) + 1):
+        if n % x == 0:
+            return False
+    return True
+
+# checks if a number (n) is pandigital
+def isPandigital(n):
+    set_n = set(int(x) for x in str(n))
+    if len(set_n) != len(str(n)): return False
+    c = len(set_n)
+    for digit in xrange(1,c+1):
+        if digit not in set_n: return False
+    return True
+
+# determines if a number is perfect (equals the sum of its proper divisors)
+def isPerfect(n):
+    return sum(getProperDivisors(n)) == n
+
+# determines if a number is deficient (sum of proper divisors < number)
+def isDeficient(n):
+    return sum(getProperDivisors(n)) < n
+
+def isPalindromic(n):
+    n_str = str(n)
+    return n_str == n_str[::-1]
+
+# determines if a number is abundant (sum of proper divisors > number)
+def isAbundant(n):
+    return sum(getProperDivisors(n)) > n
+
+# gets factors for a number
+# this can be improved via prime factorization
+def getFactors(n):
+    factors = []
+    for x in bigRange(1, (n / 2) + 1):
+        if n % x == 0:
+            factors.append(x)
+    factors.append(n)
+    return factors
+
+# gets factors for a number using prime factorization
+def getFactorsFast(n):
+    factors = {1}
+    if n == 1: return factors
+    prime_factors = getPrimeFactors(n)
+    for prime_factor in prime_factors:
+        new_factors = set([])
+        if prime_factor not in factors: new_factors.add(prime_factor)
+        for factor in factors:
+            new_factor = factor * prime_factor
+            if new_factor not in factors and new_factor not in new_factors:
+                new_factors.add(new_factor)
+        factors = factors.union(new_factors)
+    return factors
+
+# gets the proper divisors for the specified number
+def getProperDivisors(n):
+    proper_divisors = getFactorsFast(n)
+    proper_divisors.discard(n)
+    return proper_divisors
+
+# gets the multiples of n to to
+def getMultiplesTo(n, to):
+    multiple = n+n
+    while multiple < to:
+        yield multiple
+        multiple += n
+
+# gets the prime factors for a number in a list
+# the list will look like 528 => [2, 2, 2, 2, 3, 11]
+def getPrimeFactors(n):
+    primeFactors = []    
+    current = n
+    while not isPrime(current):
+        for prime in generatePrimes():
+            if current % prime == 0:
+                primeFactors.append(prime)
+                current /= prime
+                break
+    primeFactors.append(current)
+
+    return primeFactors
+
+# gets the prime factors for a number in a dictionary
+# where the key is the number and the value is the exponent
+# 528 => { 2: 4, 3: 1, 11: 1 }
+def getPrimeFactorsDictionary(n):
+    primeFactors = {}
+    current = n
+    while not isPrime(current):
+        for prime in generatePrimes():
+            if current % prime == 0:
+                if prime in primeFactors: primeFactors[prime] += 1
+                else: primeFactors[prime] = 1
+                current /= prime
+                break
+    if current in primeFactors: primeFactors[current] += 1
+    else: primeFactors[current] = 1
+
+    return primeFactors
+
+# gets the number of factors
+# using prime factorization and taking the product
+# of 1+each_exponent.
+def getFactorCount(n):
+    primeFactors = getPrimeFactorsDictionary(n)
+    factor_count = 1
+    for value in primeFactors.values():
+        factor_count *= (value + 1)
+    return factor_count
+
+# returns the sum of numbers from 1 to n
+# 1+2+...+n
+def sumTo(n):
+    return (n**2+n)/2
+
+# returns the sum of the squares from 1 to n
+# 1**2+2**2+...+n**2
+def sumOfSquaresTo(n):
+    return int(n**3/3.0+n**2/2.0+n/6.0)
+
+# gets the number of combinations of size k
+# from a group of size n
+def combinations(n, k):
+    return factorial(n)/(factorial(k)*factorial(n-k))
---
+++ tests/testEuler.py
@@ -0,0 +1,171 @@
+import unittest
+import euler
+import itertools
+
+class testBigRange(unittest.TestCase):
+    def testSmallRangeDefaultStep(self):
+        expected = list(range(0, 10))
+        actual = list(euler.bigRange(0, 10))
+        self.assertItemsEqual(expected, actual)
+
+    def testSmallRangeNonDefaultStep(self):
+        expected = list(range(2, 10, 3))
+        actual = list(euler.bigRange(2, 10, 3))
+        self.assertItemsEqual(expected, actual)
+
+    def testBigRangeDefaultStep(self):
+        expected = [1000000000000, 1000000000001, 1000000000002]
+        actual = list(euler.bigRange(1000000000000, 1000000000003))
+        self.assertItemsEqual(expected, actual)
+
+    def testBigRangeNonDefaultStep(self):
+        expected = [1000000000000, 1000000000002, 1000000000004, 1000000000006]
+        actual = list(euler.bigRange(1000000000000, 1000000000008, 2))
+        self.assertItemsEqual(expected, actual)
+
+class testReverseBigRange(unittest.TestCase):
+    def testSmallRangeDefaultStep(self):
+        expected = list(range(10, 0, -1))
+        actual = list(euler.reverseBigRange(10, 0))
+        self.assertItemsEqual(expected, actual)
+
+    def testSmallRangeNonDefaultStep(self):
+        expected = list(range(10, 2, -3))
+        actual = list(euler.reverseBigRange(10, 2, 3))
+        self.assertItemsEqual(expected, actual)
+
+    def testBigRangeDefaultStep(self):
+        expected = [1000000000003, 1000000000002, 1000000000001]
+        actual = list(euler.reverseBigRange(1000000000003, 1000000000000))
+        self.assertItemsEqual(expected, actual)
+
+    def testBigRangeNonDefaultStep(self):
+        expected = [1000000000006, 1000000000004, 1000000000002]
+        actual = list(euler.reverseBigRange(1000000000006, 1000000000001, 2))
+        self.assertItemsEqual(expected, actual)
+
+class testFibonacci(unittest.TestCase):
+    def testSuccess(self):
+        expected = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+        actual = itertools.islice(euler.fibonacci(), 10)
+        self.assertItemsEqual(expected, actual)
+
+class testGeneratePrimes(unittest.TestCase):
+    def testDefaultStart(self):
+        expected = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71]
+        actual = itertools.islice(euler.generatePrimes(), 20)
+        self.assertItemsEqual(expected, actual)
+
+    def testNonDefaultStart(self):
+        expected = [5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71]
+        actual = itertools.islice(euler.generatePrimes(4), 18)
+        self.assertItemsEqual(expected, actual)
+
+class testGetPrimesTo(unittest.TestCase):
+    def testSuccess(self):
+        expected = [2, 3, 5, 7, 11, 13, 17, 19, 23]
+        actual = euler.getPrimesTo(25)
+        self.assertItemsEqual(expected, actual)
+
+class testGenerateTriangleNumbers(unittest.TestCase):
+    def testSuccess(self):
+        expected = [1, 3, 6, 10, 15, 21, 28, 36, 45, 55]
+        actual = itertools.islice(euler.generateTriangleNumbers(), 10)
+        self.assertItemsEqual(expected, actual)
+
+class testIsTriangle(unittest.TestCase):
+    def testFailure(self):
+        self.assertFalse(euler.isTriangle(2))
+        self.assertFalse(euler.isTriangle(4))
+        self.assertFalse(euler.isTriangle(24))
+        self.assertFalse(euler.isTriangle(56))
+
+    def testSuccess(self):
+        self.assertTrue(euler.isTriangle(1))
+        self.assertTrue(euler.isTriangle(3))
+        self.assertTrue(euler.isTriangle(6))
+        self.assertTrue(euler.isTriangle(10))
+        self.assertTrue(euler.isTriangle(36))
+
+class testGeneratePentagonalNumbers(unittest.TestCase):
+    def testSuccess(self):
+        expected = [1, 5, 12, 22, 35, 51, 70, 92, 117, 145]
+        actual = itertools.islice(euler.generatePentagonalNumbers(), 10)
+        self.assertItemsEqual(expected, actual)
+
+class testIsPentagonal(unittest.TestCase):
+    def testFailure(self):
+        self.assertFalse(euler.isPentagonal(2))
+        self.assertFalse(euler.isPentagonal(3))
+        self.assertFalse(euler.isPentagonal(4))
+        self.assertFalse(euler.isPentagonal(21))
+        self.assertFalse(euler.isPentagonal(91))
+        self.assertFalse(euler.isPentagonal(118))
+
+    def testSuccess(self):
+        self.assertTrue(euler.isPentagonal(1))
+        self.assertTrue(euler.isPentagonal(5))
+        self.assertTrue(euler.isPentagonal(12))
+        self.assertTrue(euler.isPentagonal(22))
+        self.assertTrue(euler.isPentagonal(92))
+        self.assertTrue(euler.isPentagonal(145))
+
+class generateHexagonalNumbers(unittest.TestCase):
+    def testFailure(self):
+        expected = [1, 6, 15, 28, 45, 66, 91, 120, 153, 190]
+        actual = itertools.islice(euler.generateHexagonalNumbers(), 10)
+        self.assertItemsEqual(expected, actual)
+
+class testIsHexagonal(unittest.TestCase):
+    def testFailure(self):
+        self.assertFalse(euler.isHexagonal(0))
+        self.assertFalse(euler.isHexagonal(2))
+        self.assertFalse(euler.isHexagonal(3))
+        self.assertFalse(euler.isHexagonal(4))
+        self.assertFalse(euler.isHexagonal(7))
+        self.assertFalse(euler.isHexagonal(8))
+        self.assertFalse(euler.isHexagonal(14))
+        self.assertFalse(euler.isHexagonal(92))
+
+    def testSuccess(self):
+        self.assertTrue(euler.isHexagonal(1))
+        self.assertTrue(euler.isHexagonal(6))
+        self.assertTrue(euler.isHexagonal(15))
+        self.assertTrue(euler.isHexagonal(28))
+        self.assertTrue(euler.isHexagonal(45))
+        self.assertTrue(euler.isHexagonal(190))
+
+class testGetIterativeSequence(unittest.TestCase):
+    def testStartingOnEven(self):
+        expected = [50, 25, 76, 38, 19, 58, 29, 88, 44, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1]
+        actual = euler.getIterativeSequence(50)
+        self.assertItemsEqual(expected, actual)
+
+    def testStartingOnOdd(self):
+        expected = [39, 118, 59, 178, 89, 268, 134, 67, 202, 101, 304, 152, 76, 38, 19, 58, 29, 88, 44, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1]
+        actual = euler.getIterativeSequence(39)
+        self.assertItemsEqual(expected, actual)
+
+    def testStartingOnZero(self):
+        expected = [0]
+        actual = euler.getIterativeSequence(0)
+        self.assertItemsEqual(expected, actual)
+
+    def testStartingNegative(self):
+        expected = [-33]
+        actual = euler.getIterativeSequence(-33)
+        self.assertItemsEqual(expected, actual)
+
+class testIsEven(unittest.TestCase):
+    def testFailure(self):
+        self.assertFalse(euler.isEven(1))
+        self.assertFalse(euler.isEven(3))
+        self.assertFalse(euler.isEven(5))
+        self.assertFalse(euler.isEven(99))
+        self.assertFalse(euler.isEven(1344323))
+
+    def testSuccess(self):
+        self.assertTrue(euler.isEven(2))
+        self.assertTrue(euler.isEven(4))
+        self.assertTrue(euler.isEven(998))
+        self.assertTrue(euler.isEven(1024))
